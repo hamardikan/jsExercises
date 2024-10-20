1ba6014a4dcbcbe3370ed1edc5f4e9634a4b5760
adhmrdkn <a.hamardikan@gmail.com>
2024-10-20 23:11:13 +0700

    added completed exercises

--- lesson12/12-advanced-functions.js
+++ lesson12/12-advanced-functions.js
@@ -1,20 +1,86 @@
- let add = function() {
-    console.log(2+3);
- };
+function greeting(){
+   console.log("Hello!")
+}
 
- add();
+greeting();
 
- function runTwice(fun){
-        fun();
-        fun();
- }
+const num = 2;
 
- runTwice(function(){
-    console.log('12b');
- })
+const function1 = function greeting (){
+   console.log("Hello2!");
+};
 
- runTwice(add);
+console.log(function1);
+function1();
 
- setTimeout(add, 5000);
+const object1 = {
+   num: 2,
+   fun: function greeting(){
+      console.log("Hello3!");
+   }
+};
+
+object1.fun();
 
 
+function display(param){
+   console.log(param)
+}
+
+display(2);
+
+function run(param){
+   param();
+}
+run(function () {
+   console.log("hello4")
+
+   setTimeout(function (){
+      console.log("timeout");
+   }, 5000)
+});
+
+// setInterval(function(){
+//    console.log("interval");
+// }, 3000);
+
+const arrowFunction = () => {
+   console.log("Hello!")
+};
+
+arrowFunction();
+
+const oneParam = param => {
+   console.log(param + 1);
+}
+
+oneParam(2);
+
+const oneLine = () => 2 + 3;
+console.log(oneLine());
+
+const object2 = {
+   method: () => {
+
+   },
+   method() {
+
+   }
+}
+
+console.log(
+   [1, -3, 5].filter((value, index) => 
+{
+   return value >= 0;
+})
+);
+console.log(
+[ 1, 2, 3].map((value, index) => {
+   return 2 * value;
+})
+);
+
+//oneline
+console.log(
+   [1,2,3].map(value => value * 2)
+);
